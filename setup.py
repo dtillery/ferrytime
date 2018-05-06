af58dc0d974514e990f1b97890b4be8e08e3d6e7
David Tillery <dtillery@2u.com>
2018-05-06 14:57:14 -0400

    add pytest and editable install for running tests easily

---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+NAME = 'ferrytime'
+DESCRIPTION = 'times for ferries.'
+REQUIRES_PYTHON = '>=3.6.0'
+VERSION = None
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    # 'requests', 'maya', 'records',
+]
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+# If you do change the License, remember to change the Trove Classifier for that!
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    python_requires=REQUIRES_PYTHON,
+    packages=find_packages(exclude=('tests', 'tasks', 'skill', 'lambda')),
+    install_requires=REQUIRED,
+    include_package_data=True,
+)
