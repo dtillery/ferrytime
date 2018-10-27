a227bf707220f18462206c11216b0f7dc0a5a070
David Tillery <dtillery@2u.com>
2018-10-27 18:25:55 -0400

    add fallback intent (thank god)

--- lambda/handler.py
+++ lambda/handler.py
@@ -5,12 +5,19 @@ import alexandra
 
 app = alexandra.Application()
 
+
+@app.intent("AMAZON.FallbackIntent")
+def fallback(slots, context):
+    return alexandra.respond("Sorry, I didn't understand what asked for.")
+
+
 @app.intent("GetServiceAlertsIntent")
-def service_alert(event, context):
-    print("server_alert intent event is")
+def service_alert(slots, context):
+    print("server_alert intent slots are:")
     print(event)
 
     return alexandra.respond("Service Alerts Intent Response")
+
 
 def dispatch(event, context):
     print("Dispatch event is:")
